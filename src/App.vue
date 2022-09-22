c211d1f5dcede3067f15920311a3bdeb678e4f78
Will Vitorino <josewilliam@justto.com.br>
2022-09-21 22:13:14 -0300

    :art: Componentiza container base.

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,9 @@
 <template>
-  <el-container class="app-container">
-    <el-header class="app-container__header">
-      <h1>NFL Schedule</h1>
-    </el-header>
-
-    <el-main class="app-container__main">
-      <RouterView v-slot="{ Component }">
-        <transition>
-          <component :is="Component" />
-        </transition>
-      </RouterView>
-    </el-main>
-  </el-container>
+  <MainContainer/>
 </template>
 
 <script setup lang="ts">
-  import { RouterView } from 'vue-router'
+import { defineAsyncComponent } from 'vue'
+
+const MainContainer = defineAsyncComponent(() => import('@/MainContainer.vue'))
 </script>
-
-<style lang="scss" scoped>
-.app-container {
-  display: flex;
-  flex-direction: column;
-  height: 100vh;
-
-  .app-container__header {
-    display: flex;
-    justify-content: center;
-  }
-
-  .app-container__main {
-    height: 100%;
-  }
-}
-</style>
---
+++ src/MainContainer.vue
@@ -0,0 +1,30 @@
+<template>
+  <el-container class="app-container">
+    <el-header class="app-container__header">
+      <h1>NFL Schedule</h1>
+    </el-header>
+
+    <el-main class="app-container__main">
+      <RouterView v-slot="{ Component }">
+        <transition>
+          <component :is="Component"/>
+        </transition>
+      </RouterView>
+    </el-main>
+  </el-container>
+</template>
+<script setup lang="ts">
+ import { RouterView } from 'vue-router'
+</script>
+
+<style lang="scss" scoped>
+.app-container {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+
+  .app-container__header {
+    text-align: center;
+  }
+}
+</style>
