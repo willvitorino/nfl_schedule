5236b803f92ccd8eff2dc0672b9fe1b7063cd451
Will Vitorino <josewilliam@justto.com.br>
2022-09-26 20:34:56 -0300

    ♻️ Adiciona body do card e placa de pontuação.

---
+++ src/components/gameCard/partials/CardBody.vue
@@ -0,0 +1,42 @@
+<script lang="ts" setup>
+  import { defineAsyncComponent } from 'vue'
+
+  import type { Schedule } from "@/model/store/types/schedule"
+
+  import { Place } from '@element-plus/icons-vue'
+
+  const ScoreBase = defineAsyncComponent(() => import('@/components/score/ScoreBase.vue'))
+
+  defineProps<{
+    schedule: Schedule
+  }>()
+</script>
+
+<template>
+  <el-space
+    class="game-card__body"
+    alignment="center"
+    direction="vertical"
+    fill
+    wrap
+  >
+    <el-button
+      :icon="Place"
+      round
+    >
+      {{ schedule.venue }}
+    </el-button>
+
+    <ScoreBase
+      :homeTeam="schedule.homeTeam"
+      :awayTeam="schedule.awayTeam"
+    />
+  </el-space>
+</template>
+
+<style lang="scss" scoped>
+.game-card__body {
+  display: flex;
+  justify-content: center;
+}
+</style>
---
+++ src/components/score/ScoreBase.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import type { TeamGameInfo } from "@/model/store/types/team_game_info"
+
+defineProps<{
+  homeTeam: TeamGameInfo,
+  awayTeam: TeamGameInfo
+}>()
+</script>
+
+<template>
+  <el-space
+      class="score-container"
+      direction="horizontal"
+  >
+    <div class="score-container__item el-input el-input-group el-input-group--append">
+      <div class="el-input__wrapper">
+        {{ awayTeam.name }}
+      </div>
+
+      <div class="el-input-group__append">
+        {{ awayTeam.score }}
+      </div>
+    </div>
+
+    <div class="score-container__item el-input el-input-group el-input-group--prepend">
+      <div class="el-input-group__prepend">
+        {{ homeTeam.score }}
+      </div>
+
+      <div class="el-input__wrapper">
+        {{ homeTeam.name }}
+      </div>
+    </div>
+  </el-space>
+</template>
+
+<style lang="scss">
+.score-container {
+  display: flex;
+
+  .el-space__item {
+    flex: 1;
+  }
+}
+</style>
